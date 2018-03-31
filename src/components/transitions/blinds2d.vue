3914ab566c31ca0c564b4071f3636dea9d6a8c7b
ragnar <ragnar@deulos.com>
2018-03-31 11:27:24 +0200

    Started working

---
+++ src/components/transitions/blinds2d.vue
@@ -0,0 +1,56 @@
+<template>
+	<flux-grid v-if="css" :num-rows="numRows" :num-cols="numCols" :size="size" :css="css" ref="grid"></flux-grid>
+</template>
+
+<script>
+	import FluxGrid from '../grid.vue';
+
+	export default {
+		data: () => ({
+			numRows: 0,
+			numCols: 0,
+			duration: 800,
+			easing: 'linear',
+			tileDelay: 100,
+			size: {},
+			css: undefined
+		}),
+
+		components: {
+			FluxGrid
+		},
+
+		methods: {
+			run(slider, direction) {
+				this.size = slider.size;
+				this.numRows = 1;
+				this.numCols = parseInt(this.size.width / 70);
+				this.css = Object.assign({}, slider.frontImage.style);
+
+				slider.frontImage.style.visibility = 'hidden';
+
+				this.$nextTick(() => {
+					setTimeout(() => {
+						this.$refs.grid.$refs.tiles.forEach((tile, index) => {
+							Object.assign(tile.style, {
+								transition: 'all '+ this.duration +'ms '+ this.easing +' '+ (this.tileDelay * (direction === 'right'? index : this.numCols - index - 1)) +'ms',
+								opacity: '0.1',
+								transform: 'scaleX(0)'
+							});
+						});
+					}, 20);
+				});
+
+				return this.tileDelay * this.numCols + this.duration;
+			},
+
+			reset(slider) {
+				slider.rearImage.style.visibility = 'visible';
+			}
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
