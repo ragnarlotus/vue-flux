48d765bbe95db008e59e6573fdee6b175e77a8fe
deulos <ragnar@deulos.com>
2018-05-04 07:56:02 +0200

    Updated bundle command

---
+++ build/webpack.bundle.conf.js
@@ -0,0 +1,72 @@
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const webpack = require('webpack')
+const config = require('../config')
+const merge = require('webpack-merge')
+const baseWebpackConfig = require('./webpack.base.conf')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const env = require('../config/prod.env')
+
+const webpackConfig = merge(baseWebpackConfig, {
+  module: {
+    rules: utils.styleLoaders({
+      sourceMap: config.bundle.productionSourceMap,
+      extract: true,
+      usePostCSS: true
+    })
+  },
+  devtool: config.bundle.productionSourceMap ? config.bundle.devtool : false,
+  output: {
+    path: config.bundle.assetsRoot,
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    library: '[name]'
+  },
+  plugins: [
+    // http://vuejs.github.io/vue-loader/en/workflow/production.html
+    new webpack.DefinePlugin({
+      'process.env': env
+    }),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        compress: {
+          warnings: false
+        }
+      },
+      sourceMap: config.bundle.productionSourceMap,
+      parallel: true
+    }),
+    // extract css into its own file
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      // Setting the following option to `false` will not extract CSS from codesplit chunks.
+      // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
+      // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`, 
+      // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
+      allChunks: true,
+    }),
+    // Compress extracted CSS. We are using this plugin so that possible
+    // duplicated CSS from different components can be deduped.
+    new OptimizeCSSPlugin({
+      cssProcessorOptions: config.bundle.productionSourceMap
+        ? { safe: true, map: { inline: false } }
+        : { safe: true }
+    }),
+    // keep module.id stable when vendor modules does not change
+    new webpack.HashedModuleIdsPlugin(),
+    // enable scope hoisting
+    new webpack.optimize.ModuleConcatenationPlugin()
+  ]
+});
+
+webpackConfig.entry = {
+  'vue-flux': './src/index.js'
+};
+
+module.exports = webpackConfig
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import VueFlux from './components/VueFlux.vue';
+import FluxWrapper from './components/FluxWrapper.vue';
+import FluxGrid from './components/FluxGrid.vue';
+import FluxCube from './components/FluxCube.vue';
+import FluxVortex from './components/FluxVortex.vue';
+import FluxImage from './components/FluxImage.vue';
+import Transitions from './components/transitions';
+
+export default {
+	VueFlux,
+	FluxWrapper,
+	FluxGrid,
+	FluxCube,
+	FluxVortex,
+	FluxImage,
+	Transitions
+};
