b3cbcc4a39754ae76c7ff28c06cef8025760941e
ragnar <ragnar@deulos.com>
2018-05-08 07:14:51 +0200

    Added docs

--- config/index.js
+++ config/index.js
@@ -74,7 +74,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue-flux/',
 
     /**
      * Source Maps
