b81fc812a9d43d31b1815d7fdc6a793be27a435f
Oscar <ragnar@deulos.com>
2018-09-07 12:19:19 +0200

    Fixed time calculation not being good when not wider

--- src/components/transitions/blocks3d.vue
+++ src/components/transitions/blocks3d.vue
@@ -39,7 +39,7 @@
 				numCols: Math.floor(this.slider.size.width / 120)
 			});
 
-			this.totalDuration = this.tileDelay * this.numRows * 2 + this.tileDuration;
+			this.totalDuration = this.tileDelay * (this.numRows > this.numCols? this.numRows : this.numCols) * 2;
 
 			this.index = {
 				front: this.slider.currentImage.index,
