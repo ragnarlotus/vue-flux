3034177910ee306e22944476e31bb7955bdb3e55
deulos <ragnar@deulos.com>
2019-04-18 10:51:27 +0200

    Finish FluxImage revamp

--- src/components/FluxImage.vue
+++ src/components/FluxImage.vue
@@ -1,50 +1,35 @@
 <template>
-	<div :style="style" ref="image">
-		<img v-if="src && !size" :src="src" @load="setImageSize()" @error="setImageSize()">
+	<div :style="style">
+		<img v-if="imageSrc && !srcSize" :src="image.src" @load="setSrcSize()" @error="setSrcSize()">
 	</div>
 </template>
 
 <script>
-	import DisplayController from '@/controllers/Display.js';
+	import DomHelper from '@/classes/DomHelper.js';
 
 	export default {
 		name: 'FluxImage',
 
 		data: () => ({
-			loadSize: undefined,
+			srcSize: undefined,
 			baseStyle: {
 				position: 'absolute',
-				top: 0,
-				left: 0,
-				width: '100%',
-				height: '100%',
 				overflow: 'hidden',
 				backfaceVisibility: 'hidden',
-				backgroundColor: 'transparent',
 				backgroundImage: 'none',
 				zIndex: 'auto',
 			},
 		}),
 
 		props: {
-			displaySize: {
+			size: {
 				type: Object,
-				required: false,
+				default: () => ({}),
 			},
 
 			image: {
-				type: Object,
-				required: false,
-			},
-
-			imageSrc: {
-				type: String,
-				required: false,
-			},
-
-			imageSize: {
-				type: Object,
-				required: false,
+				type: [ String, Object ],
+				default: () => ({}),
 			},
 
 			color: {
@@ -62,155 +47,124 @@
 		},
 
 		computed: {
-			display: function() {
-				return this.displaySize || DisplayController.getSizeFrom(this.$el);
+			viewSize() {
+				return this.size || (new DomHelper(this.$el)).size;
 			},
 
-			src: function() {
-				if (this.image && this.image.src)
-					return this.image.src;
-
-				if (this.imageSrc)
-					return this.imageSrc;
-
-				return undefined;
+			imageSrc() {
+				return typeof this.image === 'string'? this.image : this.image.src;
 			},
 
-			size: function() {
-				if (this.image && this.image.size)
-					return this.image.size;
-
-				if (this.imageSize)
-					return this.imageSize;
-
-				if (this.loadSize)
-					return this.loadSize;
-
-				return undefined;
+			imageSize() {
+				return this.image.size || this.srcSize;
 			},
 
-			style: function() {
+			sizeStyle() {
 				return {
-					position: 'absolute',
-					top: 0,
-					left: 0,
-					width: '100%',
-					height: '100%',
-					overflow: 'hidden',
-					backfaceVisibility: 'hidden',
-					backgroundColor: this.color,
-					backgroundImage: 'none',
-					zIndex: 'auto',
+					width: this.size.width +'px' || '100%',
+					height: this.size.height +'px' || '100%',
 				};
-			}
-		}
-
-		created() {
-			this.setCss(this.css);
-		},
-
-		mounted() {
-			if (this.imageSrc)
-				this.setImageSrc(this.imageSrc);
-
-			if (this.imageSize)
-				this.setImageSize(this.imageSize);
-
-			if (this.color)
-				this.setColor(this.color);
-		},
-
-		methods: {
-			getImageSrc() {
-				return this.src;
 			},
 
-			setImageSrc(src) {
-				this.src = src;
-				this.size = this.imageSize;
-
-				this.init();
+			colorStyle() {
+				return {
+					backgroundColor: this.color,
+				};
 			},
 
-			getImageSize() {
-				return this.size;
-			},
-
-			setImageSize(size) {
-				if (size === undefined) {
-					let img = this.$refs.image.firstChild;
-
-					if (img === undefined)
-						return;
-
-					size = {
-						width: img.naturalWidth || img.width,
-						height: img.naturalHeight || img.height,
+			imageStyle() {
+				if (!this.imageSize) {
+					return {
+						backgroundImage: 'none',
 					};
 				}
 
-				this.size = {
-					...this.size,
-					...size,
-				};
-
-				this.init();
-			},
-
-			getProperties() {
-				return {
-					src: this.src,
-					size: this.size,
-				};
-			},
-
-			init() {
-				if (this.size === undefined)
-					return;
-
-				let display = this.display.size;
+				let view = this.viewSize;
 
 				let image = {
+					...this.imageSize,
 					top: 0,
 					left: 0,
-					...this.size,
-					src: 'url("'+ this.src +'")',
+					src: 'url("'+ this.imageSrc +'")',
 				};
 
-				if (image.height / image.width >= display.height / display.width) {
-					image.height = Math.ceil(display.width * image.height / image.width);
-					image.width = Math.ceil(display.width);
-					image.top = Math.ceil((display.height - image.height) / 2);
+				if (image.height / image.width >= view.height / view.width) {
+					image.height = Math.ceil(view.width * image.height / image.width);
+					image.width = Math.ceil(view.width);
+					image.top = Math.ceil((view.height - image.height) / 2);
 
 				} else {
-					image.width = Math.ceil(display.height * image.width / image.height);
-					image.height = Math.ceil(display.height);
-					image.left = Math.ceil((display.width - image.width) / 2);
+					image.width = Math.ceil(view.height * image.width / image.height);
+					image.height = Math.ceil(view.height);
+					image.left = Math.ceil((view.width - image.width) / 2);
 				}
 
 				image.top -= parseFloat(this.css.top);
 				image.left -= parseFloat(this.css.left);
 
-				this.setCss({
+				return {
 					top: 0,
 					left: 0,
 					backgroundImage: image.src,
 					backgroundSize: image.width +'px '+ image.height +'px',
 					backgroundPosition: image.left +'px '+ image.top +'px',
 					backgroundRepeat: 'no-repeat',
-				});
+				};
+			},
+
+			style() {
+				return {
+					...this.baseStyle,
+					...this.sizeStyle,
+					...this.colorStyle,
+					...this.imageStyle,
+				};
+			},
+		},
+
+		watch: {
+			css() {
+				this.setCss(this.css);
+			},
+		},
+
+		created() {
+			this.setCss(this.css);
+		},
+
+		methods: {
+			getProperties() {
+				return {
+					size: this.viewSize,
+					image: {
+						src: this.imageSrc,
+						size: this.imageSize,
+					},
+				};
+			},
+
+			setSrcSize() {
+				let img = this.$el.firstChild;
+
+				if (img === undefined)
+					return;
+
+				this.srcSize = {
+					width: img.naturalWidth || img.width,
+					height: img.naturalHeight || img.height,
+				};
 			},
 
 			setCss(css) {
-				this.style = {
-					...this.style,
+				this.baseStyle = {
+					...this.baseStyle,
 					...css,
 				};
 			},
 
 			transform(css) {
 				this.$nextTick(() => {
-					this.$refs.image.clientHeight;
+					this.$el.clientHeight;
 					this.setCss(css);
 				});
 			},
