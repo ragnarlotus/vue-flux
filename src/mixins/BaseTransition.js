5d1bf002dd9d969d16a38ce2210d2ebe8cc0b4b9
Oscar <ragnar@deulos.com>
2019-04-22 22:42:20 +0200

    Updated BaseTransition component

--- src/mixins/BaseTransition.js
+++ src/mixins/BaseTransition.js
@@ -1,15 +1,33 @@
 export default {
 	props: {
-		slider: {
+		size: {
 			type: Object,
+			required: false,
 		},
 
 		from: {
 			type: [ String, Object ],
+			required: true,
 		},
 
 		to: {
 			type: [ String, Object ],
+			required: false,
 		},
+
+		options: {
+			type: Object,
+			required: false,
+		},
+	},
+
+	computed: {
+		mask() {
+			return this.$parent.baseStyle;
+		}
+	},
+
+	created() {
+		Object.assign(this, this.options);
 	},
 }
