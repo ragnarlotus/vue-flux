a083a53fd14e150fda2217e7fb9224676068d8ad
Oscar <ragnar@deulos.com>
2019-04-25 08:18:21 +0200

    Remove direction from swipe transition

--- src/transitions/swipe.vue
+++ src/transitions/swipe.vue
@@ -1,9 +1,8 @@
 <template>
-	<flux-wrapper :css="wrapperCss" ref="wrapper">
+	<flux-wrapper ref="wrapper">
 		<flux-image
 			:image="from"
 			:size="size"
-			:css="imageCss"
 			ref="image">
 		</flux-image>
 	</flux-wrapper>
@@ -29,24 +28,7 @@
 		data: () => ({
 			totalDuration: 1400,
 			easing: 'ease-in-out',
-			wrapperCss: {},
-			imageCss: {},
 		}),
-
-		created() {
-			if (this.direction === 'left') {
-				Object.assign(this.wrapperCss, {
-					left: 'auto',
-					right: 0,
-				});
-
-				Object.assign(this.imageCss, {
-					left: 'auto',
-					right: 0,
-					width: this.size.width +'px',
-				});
-			}
-		},
 
 		mounted() {
 			this.$refs.wrapper.transform({
