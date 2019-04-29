e3c78f396ca779bd15f59ca1a3ebc34a232ef0aa
deulos <ragnar@deulos.com>
2019-04-29 13:34:13 +0200

    Updated fall transition

--- src/transitions/fall.vue
+++ src/transitions/fall.vue
@@ -1,11 +1,10 @@
 <template>
-	<flux-image :slider="slider" :image-src="imageSrc" :image-size="imageSize" :css="imageCss" ref="image"></flux-image>
+	<flux-image :image="from" :css="imageCss" ref="image"></flux-image>
 </template>
 
 <script>
+	import BaseTransition from '@/mixins/BaseTransition.js';
 	import FluxImage from '@/components/FluxImage.vue';
-
-	let vf, currentImage, nextImage;
 
 	export default {
 		name: 'transitionFall',
@@ -14,56 +13,27 @@
 			FluxImage,
 		},
 
+		mixins: [
+			BaseTransition,
+		],
+
 		data: () => ({
+			perspective: '1600px',
 			totalDuration: 1600,
 			easing: 'ease-in',
-			imageSrc: undefined,
-			imageSize: undefined,
 			imageCss: {
-				top: 0,
-				left: 0,
 				transformOrigin: 'center bottom',
-				zIndex: 12,
 			}
 		}),
 
-		props: {
-			slider: {
-				type: Object,
-				required: true,
-			},
-		},
-
-		computed: {
-			image: function() {
-				return this.$refs.image;
-			},
-		},
-
-		created() {
-			vf = this.slider;
-			currentImage = vf.Images.current;
-			nextImage = vf.Images.next;
-
-			this.imageSrc = currentImage.getImageSrc();
-			this.imageSize = currentImage.getImageSize();
-
-			vf.Transitions.setOptions(this);
-		},
-
 		mounted() {
-			currentImage.hide();
-			vf.mask.style.perspective = '1600px';
+			this.mask.perspective = this.perspective;
+			this.mask.overflow = 'visible';
 
-			this.image.transform({
+			this.$refs.image.transform({
 				transition: 'transform '+ this.totalDuration +'ms '+ this.easing,
 				transform: 'rotateX(-90deg)',
 			});
-		},
-
-		destroyed() {
-			nextImage.show();
-			vf.mask.style.perspective = 'none';
 		},
 	};
 </script>
