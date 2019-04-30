c02c2efa53976138bef081676f119a099c6231d5
deulos <ragnar@deulos.com>
2019-04-30 15:50:40 +0200

    Finished book transition update

--- src/transitions/book.vue
+++ src/transitions/book.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
-		<flux-image :image="images.front" :size="componentsSize" :css="imageCss" ref="image"></flux-image>
-		<flux-cube :images="images" :size="componentsSize" :css="cubeCss" ref="cube"></flux-cube>
+		<flux-image :image="images.front" :size="size" :css="imageCss" ref="image"></flux-image>
+		<flux-cube :images="images" :size="size" :css="cubeCss" ref="cube"></flux-cube>
 	</div>
 </template>
 
@@ -23,7 +23,7 @@
 		],
 
 		data: () => ({
-			totalDuration: 120000,
+			totalDuration: 1200,
 			easing: 'ease-out',
 			pageWidth: 0,
 			perspective: '1600px',
@@ -31,24 +31,22 @@
 				front: {},
 				back: {},
 			},
+			imageCss: {},
 			cubeCss: {
 				transformOrigin: 'left center',
 			},
-			imageCss: {
-			},
 		}),
 
-		computed: {
-			componentsSize() {
-				return {
-					...this.size,
-					width: this.size.width / 2,
-				};
-			},
-		},
-
 		created() {
-			this.cubeCss.left = this.componentsSize.width +'px';
+			let widthPx = (this.size.width / 2) +'px';
+
+			this.imageCss.width = widthPx;
+
+			Object.assign(this.cubeCss, {
+				left: widthPx,
+				width: widthPx,
+			});
+
 			this.images.front = this.from;
 			this.images.back = this.to;
 		},
@@ -57,34 +55,12 @@
 			this.mask.perspective = this.perspective;
 			this.mask.overflow = 'visible';
 
-			this.setCubeCss();
-			this.setCubeBackCss();
-/*
 			this.$nextTick(() => {
 				this.$refs.cube.transform({
 					transition: 'transform '+ this.totalDuration +'ms '+ this.easing,
 					transform: 'rotateY(-180deg)',
 				});
-			});*/
-		},
-
-		methods: {
-			setCubeCss() {
-				this.$refs.cube.setCss({
-					transformOrigin: 'right center',
-				});
-			},
-
-			setCubeBackCss() {
-				let backSide = this.$refs.cube.$refs.back[0];
-
-				let backgroundPosition = backSide.style.backgroundPosition;
-				let positionX = parseFloat(backgroundPosition.split(' ')[0]);
-
-				backSide.setCss({
-					backgroundPositionX: (positionX + this.pageWidth) +'px',
-				});
-			},
+			});
 		},
 	};
 </script>
