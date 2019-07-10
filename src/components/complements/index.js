5a9f7f8fa25ae9c49620707754ff894c51cfea91
deulos <ragnar@deulos.com>
2019-07-10 11:01:43 +0200

    Change the bundle schema

--- src/components/complements/index.js
+++
@@ -1,5 +0,0 @@
-export { default as FluxCaption } from './FluxCaption.vue';
-export { default as FluxControls } from './FluxControls.vue';
-export { default as FluxIndex } from './FluxIndex.vue';
-export { default as FluxPagination } from './FluxPagination.vue';
-export { default as FluxPreloader } from './FluxPreloader.vue';
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
+// Components
 export { default as FluxCube } from './FluxCube.vue';
 export { default as FluxGrid } from './FluxGrid.vue';
 export { default as FluxImage } from './FluxImage.vue';
@@ -7,3 +8,10 @@ export { default as FluxTransition } from './FluxTransition.vue';
 export { default as FluxVortex } from './FluxVortex.vue';
 export { default as FluxWrapper } from './FluxWrapper.vue';
 export { default as VueFlux } from './VueFlux.vue';
+
+// Complements
+export { default as FluxCaption } from './complements/FluxCaption.vue';
+export { default as FluxControls } from './complements/FluxControls.vue';
+export { default as FluxIndex } from './complements/FluxIndex.vue';
+export { default as FluxPagination } from './complements/FluxPagination.vue';
+export { default as FluxPreloader } from './complements/FluxPreloader.vue';
