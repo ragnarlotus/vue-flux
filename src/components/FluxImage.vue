5ad4a9d1695a3dccf0084bd8fab179e9d0f881ed
mark <mark@oeno.io>
2021-09-24 16:59:34 -0700

    feat: introduce picture element webp with png/jpg fallback. This is a breaking change to existing css backgroundImage.

--- src/components/FluxImage.vue
+++ src/components/FluxImage.vue
@@ -1,5 +1,10 @@
 <template>
-	<div class="flux-image" :style="style" />
+	<div class="flux-image" :style="style">
+		<picture>
+			<source :srcset="webpify_extension(image)" type="image/webp">
+			<img :src="image" alt=""/>
+		</picture>
+	</div>
 </template>
 
 <script>
@@ -42,12 +47,41 @@
 				}
 
 				return {
-					backgroundImage: `url(${this.img.src})`,
+					// backgroundImage: `url(${this.img.src})`,
 					backgroundSize: `${size.width}px ${size.height}px`,
 					backgroundPosition: `${position.left}px ${position.top}px`,
-					backgroundRepeat: 'no-repeat',
+					// backgroundRepeat: 'no-repeat',
 				};
 			},
 		},
+		methods: {
+			webpify_extension: function(filename) {
+				console.log(filename);
+				const src= filename.src;
+				console.log(src);
+				const pngJpgOrJPEG = src.substring(src.length - 4);
+				console.log(pngJpgOrJPEG);
+				if(pngJpgOrJPEG === '.jpg'){
+					const allBut = src.substr(0, src.length - 4);
+					return allBut + '.webp'
+				} else if(pngJpgOrJPEG === '.png'){
+					const allBut = src.substr(0, src.length - 4);
+					return allBut + '.webp'
+				} else if(pngJpgOrJPEG ==='jpeg') {
+					const allBut = src.substr(0, src.length - 5);
+					return allBut + '.webp'
+				} else{
+					return filename
+				}
+			},
+		}
 	};
 </script>
+
+<style scoped>
+img{
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+}
+</style>
