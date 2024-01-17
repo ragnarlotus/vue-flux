a5d88c4c9be50857cc7f816c0137cb052532408f
ragnarlotus <ragnar@deulos.com>
2024-01-17 20:08:43 +0100

    test: added excluded files and folders

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
+
+const exclude = [
+	...configDefaults.exclude,
+	'**/types.ts',
+	'**/__test__/',
+	'**/__mocks__/',
+];
 
 export default defineConfig({
 	plugins: [vue()],
@@ -7,13 +14,10 @@ export default defineConfig({
 		globals: true,
 		environment: 'happy-dom',
 		include: ['src/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
-		exclude: [
-			'**/node_modules/**',
-			'**/dist/**',
-			'**/cypress/**',
-			'**/.{idea,git,cache,output,temp}/**',
-			'**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
-			'**/types.ts',
-		],
+		exclude: exclude,
+		coverage: {
+			provider: 'v8',
+			exclude: exclude,
+		},
 	},
 });
