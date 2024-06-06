e7958b912009c29bdef59ec9a37d350fb301dba3
ragnarlotus <ragnarlotus@users.noreply.github.com>
2024-06-07 00:38:10 +0200

    chore: update eslint to flat config and deps

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,17 @@
+// @ts-check
+
 import js from '@eslint/js';
 import pluginVue from 'eslint-plugin-vue';
 import pluginVitest from 'eslint-plugin-vitest';
-import pluginTS from '@typescript-eslint/eslint-plugin';
 import pluginPrettier from 'eslint-plugin-prettier-vue';
 import vueParser from 'vue-eslint-parser';
-import tsParser from '@typescript-eslint/parser';
 import configPrettier from 'eslint-config-prettier';
+import pluginTS from 'typescript-eslint';
 
 export default [
+	js.configs.recommended,
+	...pluginTS.configs.recommended,
+	...pluginVue.configs['flat/recommended'],
 	{
 		languageOptions: {
 			ecmaVersion: 'latest',
@@ -17,9 +21,9 @@ export default [
 				ecmaFeatures: { modules: true },
 				sourceType: 'module',
 				ecmaVersion: 'latest',
-				project: './tsconfig.json',
+				project: true,
 				extraFileExtensions: ['.vue'],
-				parser: tsParser,
+				parser: pluginTS.parser,
 			},
 			globals: {
 				node: true,
@@ -28,22 +32,15 @@ export default [
 		files: ['src/**/*.js', 'src/**/*.ts', 'src/**/*.vue'],
 		ignores: ['node_modules/*', 'coverage/*', 'dist/*', 'public/*'],
 		plugins: {
-			'@typescript-eslint': pluginTS,
+			'typescript-eslint': pluginTS.plugin,
+		},
+	},
+	{
+		plugins: {
 			'prettier-vue': pluginPrettier,
 		},
-	},
-	js.configs.recommended,
-	{
-		files: ['**/*.ts'],
-		rules: {
-			...pluginTS.rules.recommended,
-		},
-	},
-	...pluginVue.configs['flat/recommended'],
-	{
 		rules: {
 			...pluginPrettier.configs.recommended.rules,
-			...configPrettier.rules,
 			'vue/require-default-prop': 'off',
 			'vue/multi-word-component-names': 'off',
 			'vue/html-indent': [
@@ -95,4 +92,5 @@ export default [
 			'vitest/no-hooks': ['error', { allow: ['beforeEach', 'afterEach'] }],
 		},
 	},
+	configPrettier,
 ];
